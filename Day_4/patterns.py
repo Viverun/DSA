d3af9497766028f5edc6a944c8f6db8d25bf97f7
Viverun <khanajamilahmed202@gmail.com>
2026-07-17 13:53:07 +0000

    Day-4 Completed with 'while-loops', 'pass-by-object-reference', 'time-space complexity', 'Few-patterns-design'

---
+++ Day_4/patterns.py
@@ -0,0 +1,40 @@
+# Square Pattern
+# for i in range(5):
+#     for j in range(5):
+#         print('*', end=' ')
+#     print()
+# Half-Pyramid
+# for i in range(1,6):
+#     for j in range(i):
+#         print('*', end=' ')
+#     print()
+
+# Numerical Pyramid
+# for i in range(1,6):
+#     for j in range(i):
+#         print(j+1, end=' ')
+#     print()
+
+# For printing same number on each line 
+# 1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5 
+# for i in range(1,6):
+#     for j in range(i):
+#         print(i, end=' ')
+#     print()
+
+# Inverse Half-Pyramid
+# for i in range(6,0, -1):
+#     for j in range(i):
+#         print('*', end=' ')
+#     print()
+
+# Inverse Numerical-Pyramid
+# for i in range(6,0, -1):
+#     for j in range(1,i):
+#         print(j, end=' ')
+#     print()
+
